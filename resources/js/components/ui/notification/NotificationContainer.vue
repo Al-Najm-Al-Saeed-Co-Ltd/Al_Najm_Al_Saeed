3c189efc17c28546c256b16c720e2332b3d03870
mrdevil786 <suhailsaeedme@gmail.com>
2025-09-06 02:36:35 +0530

    Refactor NotificationContainer.vue to streamline notification rendering by consolidating the Notification component's attributes into a single line for improved readability and maintainability.

--- resources/js/components/ui/notification/NotificationContainer.vue
+++ resources/js/components/ui/notification/NotificationContainer.vue
@@ -2,15 +2,9 @@
   <div class="fixed top-24 left-4 right-4 z-50 mx-auto max-w-7xl sm:top-4">
     <div class="flex justify-end">
       <div class="space-y-2 max-w-md w-full">
-      <Notification
-        v-for="notification in notifications"
-        :key="notification.id"
-        :message="notification.message"
-        :type="notification.type"
-        :duration="notification.duration"
-        :auto-close="notification.autoClose"
-        @close="removeNotification(notification.id)"
-      />
+        <Notification v-for="notification in notifications" :key="notification.id" :message="notification.message"
+          :type="notification.type" :duration="notification.duration" :auto-close="notification.autoClose"
+          @close="removeNotification(notification.id)" />
       </div>
     </div>
   </div>
