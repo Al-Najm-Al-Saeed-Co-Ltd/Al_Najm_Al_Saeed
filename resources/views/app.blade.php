d5f353281724013fd49e3e035cccc5021f08e97a
mrdevil786 <suhailsaeedme@gmail.com>
2025-09-09 23:15:15 +0530

    Enhance font loading performance by adding font-display property in app.css and updating font link in app.blade.php to preload for improved rendering. Added noscript fallback for better accessibility.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -36,8 +36,9 @@
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.bunny.net" crossorigin>
+        <link rel="preload" href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" as="style" onload="this.onload=null;this.rel='stylesheet'">
+        <noscript><link rel="stylesheet" href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600"></noscript>
 
         @vite(['resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
         @inertiaHead
